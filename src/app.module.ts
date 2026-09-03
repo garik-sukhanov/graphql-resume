89e3ca70c04e259bdff7d72eb5d43d3543e8a984
SukhanovIgorG <sukhanovgarik@yandex.ru>
2026-09-03 10:16:48 +0300

    feat: add graphql

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,20 +2,39 @@ import { Module } from '@nestjs/common';
 import { createObserveModule } from '@nestjs/observe';
 import { AppController } from './app.controller.js';
 import { AppService } from './app.service.js';
+import { GraphQLModule, Query, Resolver } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 
 export const { ObserveModule, ObserveInstrument } = createObserveModule();
 
+@Resolver()
+export class FooResolver {
+  @Query(() => String)
+  sayHello(): string {
+    return 'Hello World!';
+  }
+}
+
+console.log(process.env.YOUR_APP_KEY);
 @Module({
   imports: [
     // Distributed tracing, auto-correlated logs, request/job metrics, error
     // telemetry, alarms, and more — out of the box. Sign up at https://observe.nestjs.com
-    ObserveModule.forRoot({
-      appKey: 'YOUR_APP_KEY',
-      appSecret: 'YOUR_APP_SECRET',
-      serviceId: 'graphql-resume',
+    // ObserveModule.forRoot({
+    //   appKey: process.env.YOUR_APP_KEY || 'YOUR_APP_KEY',
+    //   appSecret: process.env.YOUR_APP_SECRET || 'YOUR_APP_KEY',
+    //   serviceId: process.env.SERVICE_ID || 'YOUR_APP_KEY ',
+    // }),
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      installSubscriptionHandlers: true,
+      autoSchemaFile: true,
+      graphiql: {
+        url: '/graphql',
+      },
     }),
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, FooResolver],
 })
 export class AppModule {}
