5e4688e8825289c83c6f0bb704d4ea5f36676536
SukhanovIgorG <sukhanovgarik@yandex.ru>
2026-09-07 16:09:08 +0300

    refactor: add apollo sendbox

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,17 +6,21 @@ import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { PrismaModule } from './prisma/prisma.module.js';
 import { ProfileModule } from './profile/profile.module';
 import { SkillModule } from './skill/skill.module';
+import { join } from 'path';
+import { ApolloServerPluginLandingPageLocalDefault } from '@apollo/server/plugin/landingPage/default';
 
 @Module({
   imports: [
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
-      installSubscriptionHandlers: true,
-      autoSchemaFile: true,
-      graphiql: {
-        url: '/graphql',
-      },
-      playground: true,
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+      sortSchema: true,
+      graphiql: false,
+      plugins: [
+        ApolloServerPluginLandingPageLocalDefault({
+          embed: true,
+        }),
+      ],
     }),
     PrismaModule,
     ProfileModule,
